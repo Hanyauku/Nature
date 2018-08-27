0d750f70088135d55817844a36c94a5b0c83acd3
Hanyauku <kochneva.p@gmail.com>
2018-08-27 07:34:03 +0200

    add model functions

--- application/models/Nature.php
+++ application/models/Nature.php
@@ -3,7 +3,23 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Nature extends CI_Model {
     public function findUser($argument) {
-        $query = "SELECT users.id, users.name FROM users WHERE email = ?;";
+        $query = "SELECT users.id, users.name, FROM users WHERE email = ?;";
         return $this->db->query($query, $argument)->row_array();
+    }
+
+    public function findCoordinates($argument) {
+        $query = "SELECT location.coordinates, users_has_location.sqm
+        FROM location
+        INNER JOIN users_has_location
+        ON users_has_location.location_id = location.id
+        WHERE users_has_location.users_id = ?";
+        return $this->db->query($query, $argument)->result_array();
+    }
+
+    public function findMedia($argument) {
+        $query = "SELECT media.link
+        FROM media
+        WHERE media.location_id = ?";
+        return $this->db->query($query, $argument)->result_array();
     }
 }
