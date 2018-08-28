2eff36e64489c55426d55ea4ba1cb1d58a4c8476
Hanyauku <kochneva.p@gmail.com>
2018-08-28 09:09:54 +0200

    merge frontend for first page

--- application/views/nature/index.php
+++ application/views/nature/index.php
@@ -1,47 +1,72 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
+ob_start();
 ?>
 
-<!DOCTYPE html>
-<html>
 <head>
-	<title>Index</title>
-	<meta charset="utf-8">
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-	<link rel="stylesheet" href="/css/style.css">
+<title>Adopt A Tree</title>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<!-- Bootstrap -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="/css/mijnstyle.css"/>
 </head>
+
 <body>
-  		<div class="row">
-    		<div class="col">
-				<div id="map"></div>
-			    <script>
-			      var map;
-			      function initMap() {
-			        map = new google.maps.Map(document.getElementById('map'), {
-			          center: {lat: 10.037054, lng: -83.350640},
-			          zoom: 8,
-					  mapTypeId: 'satellite'
-			        });
-			      }
-			    </script>
-    		</div>
-    		<div class="col">
-    		<div class="content">
-      			<h4>Take a look at your piece of rainforest</h4>
-      			<p>Are you one of our heroes and adopted a square meter of costa rican rainforest? Fly to it and explore!</p>
-      			<form action="data/searchFirst" method="post">
-					<?php if(!empty($this->session->flashdata('input_error'))) {
-                        echo $this->session->flashdata('input_error') . '<br />';
-                    }?>
-      				<input type="email" name="email" placeholder="Enter your email">
-      				<button type="submit" class="btn btn-primary mb-2">FIND</button>
-      			</form>
-      			<a href="http://www.adopteerregenwoud.nl/nl/adopteer/adopteer-regenwoud"><button type="submit" class="btn btn-primary">ADOPT A SQUARE METER FOR ONLY €2.50</button></a>
-      		</div>
-    		</div>
-  		</div>
-	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAICFxqTR1DqfTU7GzNyxRyJTNYrCSaCao&callback=initMap"
-	async defer></script>
+<!-- ====sidebar====== -->
+<div class="w3-sidebar w3-bar-block w3-green w3-animate-left"  style="width:390px" id="mySidebar">
+    <!-- clos bottun -->
+    <button class="w3-bar-item w3-button w3-large" style="text-align: center;"onclick="w3_close()">Close &times;</button>
+    <!-- sidcontaint -->
+    <div class="sidcontaint">
+        <h4><b>Take a look at your piece of rainforest</b></h4>
+        <p>Are you one of our heroes and adopted a square meter<br> of costa rican rainforest? Fly to it and explore!</p>
+
+        <form class="form-inline" action="data/searchFirst" method="post">
+            <?php if(!empty($this->session->flashdata('input_error'))) { ?>
+                <h5> <?= $this->session->flashdata('input_error') ?> </h5> <?php ;
+            }?>
+            <input type="email" class="form-control" id="email" placeholder="Enter your email" name="email">
+            <button type="submit" class="btn btn-default">Find</button>
+        </form>
+        <a href="http://www.adopteerregenwoud.nl/nl/adopteer/adopteer-regenwoud" class="btn " role="button">ADOPT A SQUARE METER FOR ONLY €2.50</a>
+
+    </div>
+</div>
+<!-- page containt with map -->
+<div class="container">
+    <div id="map"></div>
+    <button class="btn" onclick="w3_open()">&#9776;</button>
+
+</div>
+
+
+
+<script>
+function w3_open() {
+    document.getElementById("mySidebar").style.display = "block";
+}
+function w3_close() {
+    document.getElementById("mySidebar").style.display = "none";
+}
+</script>
+
+<!-- google map  -->
+<script>
+        var map;
+        function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: 10.037054, lng: -83.350640},
+            zoom: 8,
+            mapTypeId: 'satellite'
+        });
+        }
+</script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB6nhveJrJGLPkqa6gpSgbQVyssBWM63oc&callback=initMap"
+async defer></script>
+
 </body>
-</html>
